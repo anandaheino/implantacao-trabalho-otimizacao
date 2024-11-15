dfaffdee27595f42c2df95d66856f7f2a9a342e6
Lucas Ferreira <lucasareais.aluno@unipampa.edu.br>
2024-11-15 20:33:08 -0300

    Atualiza arquivos no diretório App

--- App/gerar posição gps.py
+++ App/gerar posição gps.py
@@ -64,5 +64,5 @@ def processar_arquivo(file_name, output_name):
     print(f"Processamento do arquivo '{file_name}' completo. Progresso salvo em '{output_name}'.")
 
 # Processar os arquivos de candidatos e escolas
-processar_arquivo('candidatos_1000.csv', 'candidatos_1000_gps.csv')
-#processar_arquivo('escolas_300.csv', 'escolas_300_gps.csv')
+processar_arquivo('candidatos_3000.csv', 'candidatos_3000_gps.csv')
+processar_arquivo('escolas_150.csv', 'escolas_150_gps.csv')
